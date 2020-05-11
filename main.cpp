a4bc5fa8e3c0d51e329c6143ad8b0a9fe6498717
Otavio Silva <otavioolsilva@gmail.com>
2020-05-11 17:21:08 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    double rSalario, rServico, rCapital, gMedico, gEducacao;
+
+    //Leitura dos dados
+    cout << "Renda anual com salario: ";
+    cin >> rSalario;
+    cout << "Renda anual com prestacao de servico: ";
+    cin >> rServico;
+    cout << "Renda anual com ganho de capital: ";
+    cin >> rCapital;
+    cout << "Gastos medicos: ";
+    cin >> gMedico;
+    cout << "Gastos educacionais: ";
+    cin >> gEducacao;
+
     return 0;
 }
